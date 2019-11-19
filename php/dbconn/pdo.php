dcdbbf1b9f3aa9c127478859086b3f95194e3ebd
Your Name <you@example.com>
2019-11-19 16:07:02 +0530

    1

--- php/dbconn/pdo.php
+++ php/dbconn/pdo.php
@@ -8,11 +8,11 @@ define('DB_NAME','test');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
+	$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
 }
 catch (PDOException $e)
 {
-exit("Error: " . $e->getMessage());
+	exit("Error: " . $e->getMessage());
 }
 
 $fname="hari";
@@ -23,8 +23,6 @@ $query = $dbh -> prepare($sql);
 $query->bindParam(':fname',$fname);
 $query->bindParam(':lname',$lname);
 $query->execute();
-
-
 
 
 
---
+++ php/session/define.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+$_SESSION['name']="Hari";
+$_SESSION['lname']="Patel";
+$_SESSION['city']="anand";
+
+
+// sesssion variable can be shared to one page to another page
+
+?>
