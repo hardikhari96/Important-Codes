1cf5247dc32c81cac0d7f5d4b4bba273dcb8a6cf
Your Name <you@example.com>
2019-11-19 17:39:18 +0530

    1

---
+++ php/OOP/scoperesolution/callingcalssfunctionwithoutcreatingobject.php
@@ -0,0 +1,33 @@
+<?php
+
+class flowers {
+
+	public static $name= "this is flower class";
+	public $nonstat= "this is flower class";
+
+	public function rose (){
+
+
+		echo "this is rose";
+	}
+
+	public static function lily(){
+
+		echo "this is lily";
+	}
+
+
+}
+
+
+flowers::lily(); //calling functions
+echo "<br>";
+flowers::rose();  // you can call non static function but not non static property of class using scope resolution `::`
+echo "<br>";
+
+echo flowers::$name; // accessing property of static value only , we 
+
+//this is not possible in non static property
+
+echo flowers::$nonstat;
+?>
