d1aa3b25800838749e8464210d2d0485b7c317fa
Your Name <you@example.com>
2019-11-19 11:20:54 +0530

    1

---
+++ php/header function/readpdf.php
@@ -0,0 +1,17 @@
+<?php
+/*
+header("Content-type:application/pdf");
+
+// It will be call and name the pdf file and view in browser
+header("Content-Disposition:inline;filename=Newfile.pdf");
+
+header("Content-title:Hari");
+
+// The PDF source is in pdf file
+readfile("SDLC_ Understand the Software Development Life Cycle.pdf");
+
+*/
+?>
+
+
+@echo "Hello";
