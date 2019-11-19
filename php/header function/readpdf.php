59fd6b2fc6967a8f1eb80d41fb278a331698a523
Your Name <you@example.com>
2019-11-19 11:21:30 +0530

    1

--- php/header function/readpdf.php
+++ php/header function/readpdf.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 header("Content-type:application/pdf");
 
 // It will be call and name the pdf file and view in browser
@@ -10,8 +10,6 @@ header("Content-title:Hari");
 // The PDF source is in pdf file
 readfile("SDLC_ Understand the Software Development Life Cycle.pdf");
 
-*/
 ?>
 
 
-@echo "Hello";
