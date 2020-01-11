8e517386a419b333138546c984b243bf4aff67af
Harikrushna Patel <hp@denimsoftware.net>
2020-01-11 16:15:45 +0530

    usestatement

--- php/OOP/namespace/index.php
+++ php/OOP/namespace/index.php
@@ -15,8 +15,11 @@ echo "</br>";
 
 include_once "config2.php";
 
+// namespace space; this is name space in config 2 file and it has same class name mobile which have config file 1
 
-$obj2 = new space\mobile();
+use space\mobile as newmobile;  // use statement use for naming the namespace class to new name after import
+
+$obj2 = new newmobile();
 
 $obj2 -> Samsung();
 
